d00039a1ba48551afcf71770dbf690f661b9f509
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-10-02 21:41:16 +0530

    Create Even9to100.java

---
+++ Day2/Even9to100.java
@@ -0,0 +1,14 @@
+public class Even9to100 {
+    public static void main(String[] args) {
+        int i;
+        for(i=9;i<=100;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+                
+            }
+        }
+    }
+    
+}
