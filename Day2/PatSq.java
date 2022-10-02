5a14703dde05a91812e6886ee43e18f2eb148c12
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-10-02 21:40:31 +0530

    Create PatSq.java

---
+++ Day2/PatSq.java
@@ -0,0 +1,10 @@
+public class PatSq {
+    public static void main(String[] args) {
+        int line=1;
+        while(line<=4)
+        {
+            System.out.println("* * * *");
+            line++;
+        }
+    }
+}
