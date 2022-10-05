286c4cfae911462672ef3b34b68acfcea961b722
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-10-05 23:39:20 +0530

    Update Stringinout.java

--- Day 5/Stringinout.java
+++ Day 5/Stringinout.java
@@ -1,3 +1,19 @@
+/*
+Problem:- To take input of name and age and display it
+used scanner class to input the string and age
+
+Sample Input:- 
+
+Enter your name
+Devyansh Rajput
+Enter your age
+21
+
+Sample Out:- 
+Entered Name:- Devyansh Rajput
+Entered Age:- 21
+
+*/
 import java.util.Scanner;
 
 public class Stringinout {
