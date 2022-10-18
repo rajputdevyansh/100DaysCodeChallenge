cd0b1f13623bafa7f89ecb9400af7a07ef640095
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-10-19 03:51:33 +0530

    Create Exp14.java

---
+++ Day19/Exp14.java
@@ -0,0 +1,29 @@
+/*
+Write a program to count all vowels from a sentence.
+*/
+import java.util.Scanner;
+
+public class Exp14
+{
+    static int countVowels(String str){
+        String brandNewString = str.toLowerCase();
+    
+        int i=0, count = 0;
+        while(i<brandNewString.length()){
+          char ch = brandNewString.charAt(i);
+          if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            count++;
+          }
+          i++;
+        }
+        return count;
+      }
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Enter the statement ");
+        String st = sc.nextLine();
+        sc.close();
+        System.out.println("The Number of vowels = " +countVowels(st));
+      }
+}
