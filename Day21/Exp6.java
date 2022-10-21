1b7cc483cabc87513e496587dd4c0d40c2645973
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-10-21 21:48:04 +0530

    Create Exp6.java

---
+++ Day21/Exp6.java
@@ -0,0 +1,23 @@
+/*
+
+*/
+public class Exp6 
+{
+    public static void main(String[] args) 
+    {
+        int a,b;
+        for(int i =1;i<=1000;i++)
+        {
+            if(i%3==0)
+            {
+                System.out.println("No Divisible by 3:- "+i);
+            }
+            if(i%7==0)
+            {
+                System.out.println("No Divisible by 7:- "+i);
+            }
+        }
+        
+    }
+    
+}
