564176ddff48b52a2625916b64a3adb422d407c1
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-02 22:48:38 +0530

    Update Writeafunction.py

--- Day33/Writeafunction.py
+++ Day33/Writeafunction.py
@@ -12,7 +12,7 @@ def is_leap(year):
     if(year % 400 ==0):
         return True
     if(year % 100 ==0):
-        return True
+        return False
     if(year % 4 ==0):
         return True
     else:
