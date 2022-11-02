f4b1d6600b9905e7541459480e20650b601ae2d2
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-02 22:18:10 +0530

    Create Writeafunction.py

---
+++ Day33/Writeafunction.py
@@ -0,0 +1,31 @@
+"""
+Task
+Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
+Input Format
+Read , the year to test.
+Output Format
+The function must return a Boolean value (True/False). Output is handled by the provided code stub.
+"""
+def is_leap(year):
+    leap = False
+    if(year % 400 ==0):
+        return True
+    if(year % 100 ==0):
+        return True
+    if(year % 4 ==0):
+        return True
+    else:
+        return False
+    
+    return leap
+year = int(input())
+print(is_leap(year))
+"""
+Compiler Message
+Success
+Input (stdin)
+1990
+Expected Output
+False
+"""
