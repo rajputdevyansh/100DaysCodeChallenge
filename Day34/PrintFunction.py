a5ad739f3354d722f1ab7bc8d0f31467c45ce783
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-03 22:36:21 +0530

    Create PrintFunction.py

---
+++ Day34/PrintFunction.py
@@ -0,0 +1,24 @@
+"""
+The included code stub will read an integer, , from STDIN.
+Without using any string methods, try to print the following:
+Note that "" represents the consecutive values in between.
+Example
+Print the string .
+Input Format
+The first line contains an integer .
+Constraints
+Output Format
+Print the list of integers from  through  as a string, without spaces.
+"""
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end="")
+"""
+Compiler Message
+Success
+Input (stdin)
+3
+Expected Output
+123
+"""
