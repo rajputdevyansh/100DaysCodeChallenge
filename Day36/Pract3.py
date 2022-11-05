1555ba9c75a2aa12675ee19747a13266b8b2ed70
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-05 22:50:29 +0530

    Create Pract3.py

---
+++ Day36/Pract3.py
@@ -0,0 +1,16 @@
+#create a fuction
+print("~~~ Fuction ~~~")
+
+def yes():
+    print("Take one step to the right and stomp.")
+    print("Take one step to the left and stomp.")
+    print("Shake those hips!")
+
+yes()
+"""
+Output
+~~~ Fuction ~~~
+Take one step to the right and stomp.
+Take one step to the left and stomp.
+Shake those hips!
+"""
