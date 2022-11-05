334e22fdd029f0ee7e3e4432acf3a1be35f9b761
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-05 22:57:55 +0530

    Create Pract6.py

---
+++ Day36/Pract6.py
@@ -0,0 +1,10 @@
+# Prints out the name of a favorite city
+def favorite_city(name):
+    print("One of my favorite city is", name)
+
+favorite_city("Dehradun")
+
+"""
+Output 
+One of my favorite city is Dehradun
+"""
