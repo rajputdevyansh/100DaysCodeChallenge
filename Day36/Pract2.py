150dc20f76bfc779e71840dddc27345287e85e57
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-05 22:49:55 +0530

    Create Pract2.py

---
+++ Day36/Pract2.py
@@ -0,0 +1,20 @@
+#create a if else program
+guess = input("What's my favorite food? ")
+
+if guess =="pizza":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That’s not it!")
+
+print("Thanks for playing!")
+"""
+Output 1 
+What's my favorite food? pizza
+Yep! So amazing!
+Thanks for playing!
+
+Output 2
+What's my favorite food? dosa
+Yuck! That’s not it!
+Thanks for playing!
+"""
