6bbe6b6a377122776f4fa607b4231839d3ce1416
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-05 22:47:16 +0530

    Create SimpleProg1.py

---
+++ Day36/SimpleProg1.py
@@ -0,0 +1,26 @@
+#create an if else program
+print("Hi!")
+name = input("What's your name? ")
+
+print("It's nice to meet you,", name)
+answer = input("Are you enjoying the course? ")
+
+if answer == "Yes":
+    print("That's good to hear!")
+else:
+    print("Oh no! That makes me sad!")
+"""
+Output 1
+Hi!
+What's your name? Devyansh
+It's nice to meet you, Devyansh
+Are you enjoying the course? Yes
+That's good to hear!
+
+Output 2
+Hi!
+What's your name? shiva
+It's nice to meet you, shiva
+Are you enjoying the course? No
+Oh no! That makes me sad!
+"""
