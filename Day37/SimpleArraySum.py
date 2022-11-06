f8a8fcd3a56f2040f489ea3cc8a796f155e99a61
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-06 22:39:35 +0530

    Create SimpleArraySum.py

---
+++ Day37/SimpleArraySum.py
@@ -0,0 +1,57 @@
+"""
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers representing the array's elements.
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    sum=0
+    for i in range(len(ar)):
+        sum+=ar[i]
+    return(sum)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+    
+"""
+Compiler Message
+Success
+Input (stdin)
+
+6
+1 2 3 4 10 11
+Expected Output
+
+31
+"""
