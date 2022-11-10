30ad250359ec79ab4c782b09781e4b04aa2fccfe
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-10 23:48:22 +0530

    Create Draw2.py

---
+++ Day41/Draw2.py
@@ -0,0 +1,17 @@
+# A program to create a circle
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.title("Drawing")
+my_turtle = turtle.Turtle()
+my_turtle.color("red")
+my_turtle.pensize(15)
+my_turtle.fillcolor("white")
+my_turtle.begin_fill()
+my_turtle.circle(100)
+my_turtle.end_fill()
+turtle.done()
+"""
+output added in same folder
+"""
