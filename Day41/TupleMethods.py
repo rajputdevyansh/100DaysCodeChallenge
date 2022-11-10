f3b31e81a1d29dcbce448f09c839ebf092f1e0b1
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-10 17:51:23 +0530

    Create TupleMethods.py

---
+++ Day41/TupleMethods.py
@@ -0,0 +1,16 @@
+# A program to demonstrate all tuple pre defined methods
+# count() method returns the number of times a specified value appears in the tuple.
+t1 = (1, 2, 5, 8, 7, 4, 6, 9, 5, 5, 0, 8, 5)
+x1 = t1.count(5)
+print(x1)
+"""
+4
+"""
+
+# index() method finds the first occurrence of the specified value.
+t2 = (1, 2, 5, 8, 7, 4, 6, 9, 5, 5, 0, 8, 5)
+x2 = t2.index(8)
+print(x2)
+"""
+3
+"""
