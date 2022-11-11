d84e7591cd2c999ee86e10eefbdaa52585518b89
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-11 20:51:56 +0530

    Create Draw6.py

---
+++ Day42/Draw6.py
@@ -0,0 +1,13 @@
+# program to draw hexagon
+import turtle
+tur = turtle.Turtle()
+ns = 6
+sl = 150
+angle = 360.0/ns
+for i in range(ns):
+    tur.forward(sl)
+    tur.left(angle)
+turtle.done()
+"""
+output file added
+"""
