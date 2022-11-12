de7c6ac8581e0799bad5f7a7fe7a40f2052828db
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-12 17:09:39 +0530

    Create max2.py

---
+++ Day43/max2.py
@@ -0,0 +1,29 @@
+# gfg practice list(Basic)
+# program for Maximum of two numbers
+print("Enter two numbers to compare")
+a = input('Number 1:- ')
+b = input('Number 2:- ')
+if a == b:
+    print(f'Number 1: {a} & Number 2: {b} are equal!')
+elif a > b:
+    print(f'Number 1: {a} is greater')
+else:
+    print(f'Number 2: {b} is greater')
+"""
+Enter two numbers to compare
+Number 1:- 10
+Number 2:- 12
+Number 2: 12 is greater
+"""
+"""
+Enter two numbers to compare
+Number 1:- 9
+Number 2:- 8
+Number 1: 9 is greater
+"""
+"""
+Enter two numbers to compare
+Number 1:- 25
+Number 2:- 25
+Number 1: 25 & Number 2: 25 are equal!
+"""
