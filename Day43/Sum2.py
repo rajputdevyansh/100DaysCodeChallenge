4b777d686fd792c06f1ecec6732467dc84105e00
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-12 16:58:34 +0530

    Create Sum2.py

---
+++ Day43/Sum2.py
@@ -0,0 +1,13 @@
+# gfg practice list(Basic)
+# program to add two numbers
+print('Enter numbers to get the sum')
+a = int(input('Number 1:- '))
+b = int(input('Number 2:- '))
+s = a+b
+print(f'Sum of entered numbers:- {s}')
+"""
+Enter numbers to get the sum
+Number 1:- 10
+Number 2:- 14
+Sum of entered numbers:- 24
+"""
