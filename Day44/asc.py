ebf728d46344c16eaa60d345d17109724f0fb822
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-13 19:22:17 +0530

    Create asc.py

---
+++ Day44/asc.py
@@ -0,0 +1,11 @@
+# gfg practice list(Basic)
+# Program to print ASCII Value of a character
+print('Enter Character to get ASCII value')
+c = input('Enter Character:- ')
+a = ord(c)
+print(f'ASCII value of entered character:- {a}')
+"""
+Enter Character to get ASCII value
+Enter Character:- d
+ASCII value of entered character:- 100
+"""
