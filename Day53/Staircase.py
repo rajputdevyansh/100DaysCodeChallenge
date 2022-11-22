c2955eae122770f407b2d0ab4a5ea68531aa0fa3
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-11-22 17:47:56 +0530

    Create Staircase.py

---
+++ Day53/Staircase.py
@@ -0,0 +1,57 @@
+"""
+Staircase detail
+This is a staircase of size :
+
+   #
+  ##
+ ###
+####
+Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+Write a program that prints a staircase of size.
+Function Description
+Complete the staircase function in the editor below.
+staircase has the following parameter(s):
+int n: an integer
+Print a staircase as described above.
+Input Format
+A single integer, , denoting the size of the staircase.
+
+Output Format
+Print a staircase of size  using # symbols and spaces.
+Note: The last line must have  spaces in it.
+"""
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(1, n+1):
+        print(' '*(n-i)+'#'*i)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
+"""
+Compiler Message
+Success
+Input (stdin)
+
+6
+Expected Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+"""
