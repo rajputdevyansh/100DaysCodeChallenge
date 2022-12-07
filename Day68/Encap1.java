3de2a17c8fb7f38f9e1a817278aa6fb41f3fd2f6
Devyansh Rajput <114766723+rajputdevyansh@users.noreply.github.com>
2022-12-07 22:41:30 +0530

    Update Encap1.java

--- Day68/Encap1.java
+++ Day68/Encap1.java
@@ -1,13 +1,28 @@
-package com.javatpoint;  
-public class Student{  
-//private data member  
-private String name;  
-//getter method for name  
-public String getName(){  
-return name;  
-}  
-//setter method for name  
-public void setName(String name){  
-this.name=name  
-}  
-}  
+class Area {
+
+  // fields to calculate area
+  int length;
+  int breadth;
+
+  // constructor to initialize values
+  Area(int length, int breadth) {
+    this.length = length;
+    this.breadth = breadth;
+  }
+
+  // method to calculate area
+  public void getArea() {
+    int area = length * breadth;
+    System.out.println("Area: " + area);
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+
+    // create object of Area
+    // pass value of length and breadth
+    Area rectangle = new Area(5, 6);
+    rectangle.getArea();
+  }
+}
